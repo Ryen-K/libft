9caed519cb9676dabcf40f8bca8a8ef6192e8f22
Ryen Kamkoum <rkamkoum@c2r9s10.42lausanne.ch>
2025-10-18 19:00:47 +0200

    libc: ft_bzero.c + libft.h

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,46 +6,48 @@
 /*   By: rkamkoum <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 17:08:35 by rkamkoum          #+#    #+#             */
-/*   Updated: 2025/10/18 17:39:33 by rkamkoum         ###   ####lausanne.ch   */
+/*   Updated: 2025/10/18 18:59:30 by rkamkoum         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-void	*ft_memset(void *s, int	c, size_t n);
+//#include <stdio.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
-}
+	unsigned char	*res;
+	size_t			i;
 
+	i = 0;
+	res = (unsigned char *)s;
+	while (i < n)
+	{
+		res[i] = 0;
+		i++;
+	}
+}
+/*
 int	main(void)
 {
-	char	*buffer;
+	char	buffer[] = "test";
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 0;
-	buffer = "tests only";
 	printf("before:\n");
-	while (i < 10)
+	while (buffer[i])
 	{
 		printf("%zu: %c\n", i, buffer[i]);
 		i++;
 	}
 
-	ft_bzero(buffer, sizeof(char) * 5);
-	ft_bzero(buffer+5, sizeof(char));
-	ft_bzero(buffer+6, sizeof(char) * 4);	
-
+	ft_bzero(buffer+2, sizeof(char) * 2);
 	printf("after:\n");
-	    while (j < 10)
+	    while (buffer[j])
     {
         printf("%zu: %c\n", j, buffer[j]);
         j++;
     }
 	return (0);
-}
-
-// TO BE CONTINUED
+}*/
---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkamkoum <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 17:55:01 by rkamkoum          #+#    #+#             */
+/*   Updated: 2025/10/18 19:00:08 by rkamkoum         ###   ####lausanne.ch   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+#endif

ea8aa1ad513e5e770584e992feea5b6a113b46dd
Ryen Kamkoum <rkamkoum@c2r9s10.42lausanne.ch>
2025-10-18 14:12:30 +0200

    libc: ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkamkoum <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 13:40:16 by rkamkoum          #+#    #+#             */
+/*   Updated: 2025/10/18 14:12:08 by rkamkoum         ###   ####lausanne.ch   */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+/*
+int	main(void)
+{
+	printf("%lu \n",ft_strlen(""));
+    printf("%lu \n",ft_strlen("driss"));
+    printf("%lu \n",ft_strlen("six-cent-soixante-six"));
+    printf("%lu \n",ft_strlen("j'suis a court d'idees"));
+	return (0);
+}*/

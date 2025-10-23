8c1e80e007d0bfec89aa9e0c07fbea7d68124c6c
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-23 10:33:21 +0200

    libc: ft_memcpy.c + libft.h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*ptr_dest;
+	const unsigned char	*ptr_src;
+
+	ptr_dest = (unsigned char*)dest;
+	ptr_src = (unsigned char*)src;
+	while (n--)
+		*(ptr_dest++) = *(ptr_src++);
+	return (dest);
+}

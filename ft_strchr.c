2b7c7e7b4af03a3997020e0fb66c94a8ac2dc11a
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-26 18:44:00 +0100

    libc: ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <stdio.h>
+#include "libft.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (0);
+}
+/*
+int	main(void)
+{
+	char	*str;
+	char	ch;
+	char	*res;
+//
+	str = "hello";
+	ch = 'l';
+	res = ft_strchr(str, ch);
+	printf("first occ of %c is %s\n", ch, res);
+	return (0);
+}*/

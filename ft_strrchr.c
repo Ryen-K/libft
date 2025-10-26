65b5ef738b7649dab7fadbc25cef293b1d893d1c
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-26 19:08:33 +0100

    libc: ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <stdio.h>
+#include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)s + len);
+		len--;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+        char    *str = "abcdefghijklmnopqrstuvwxyz";
+        char    ch = 'l';
+        char    *res;
+
+        res = ft_strrchr(str, ch);
+        printf("last occ of '%c':  %s\n", ch, res);
+        return (0);
+}*/
--- libft.h
+++ libft.h
@@ -27,6 +27,8 @@ int		ft_tolower(int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);

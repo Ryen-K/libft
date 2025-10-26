5b4f772c117aa6514a3c627eaaafbbdfd587f9e7
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-26 21:59:02 +0100

    libc: ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && s1[i] && s1[i] == s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*
+int	main(void)
+{
+	printf("result = %d (exp 0) \n", ft_strncmp("hello1", "hello2", 3));
+	printf("result = %d (exp pos)\n", ft_strncmp("hello7", "hello2", 6));
+	printf("result = %d (exp neg)\n", ft_strncmp("hello1", "hello2", 12));
+	printf("result = %d (exp 0) \n", ft_strncmp("hello", "hello", 5));
+	return (0);
+}*/

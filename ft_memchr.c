a61f67ff43d75880a5d64ff4bc08617396b87bdf
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-29 19:00:32 +0100

    libc: ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,4 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+
+	p = s;
+	while (n--)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return (0);
+}
+/*
+int     main(void)
+{
+        char    str[] = "hello";
+        char    ch = 'z';
+        char    *res;
+
+        res = ft_memchr(str, ch, 5);
+	if (res)
+		printf("first occ of %c is %s\n", ch, res);
+        else
+		printf("%c not found in %s \n", ch, str);
+	return (0);
+}*/

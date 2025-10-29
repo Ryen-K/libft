b292e58171a9e73f533bb04a07f02b6a516e9516
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-29 20:24:26 +0100

    libc: ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (i < len && big[i])
+	{
+		j = 0;
+		while (i + j < len && big[i + j] && little[j] == big[i + j])
+		{
+			if (little[j + 1] == '\0')
+				return ((char *)(big + i));
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	char	test_str[] = "this is an example";
+
+	printf("%s", ft_strnstr(test_str, "example", 12));
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -30,6 +30,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);

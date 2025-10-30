4a587899fcd84eb2c4b25e42d5401650abc07765
Ryen-K <ryen.kamkoum@gmail.com>
2025-10-30 19:20:59 +0100

    libc: ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || (nptr[i] == 32))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] <= '9' && nptr[i] >= '0')
+	{
+		res = (res * 10) + (nptr[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_atoi(" ---+--+1234ab567"));
+	printf("%d\n", ft_atoi(" +1234ab567"));
+	printf("%d\n", ft_atoi(" -1234ab567"));
+	printf("%d\n", ft_atoi(" +i1234ab567"));
+	printf("%d\n", ft_atoi(" 123456789"));
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int	ft_atoi(const char *nptr);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);

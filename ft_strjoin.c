6c98cbd9cb423236ab66a8cf4d6c2c302da0fce9
Ryen-K <ryen.kamkoum@gmail.com>
2025-11-01 13:43:17 +0100

    part2: ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_1;
+	size_t	len_2;
+	char	*dest;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_1 = ft_strlen(s1);
+	len_2 = ft_strlen(s2);
+	dest = ft_calloc(len_1 + len_2 + 1, sizeof(char));
+	if (!dest)
+		return (NULL);
+	ft_memcpy(dest, s1, len_1);
+	ft_memcpy(dest + len_1, s2, len_2);
+	return (dest);
+}
+/*
+int	main(void)
+{
+	printf("%s \n", ft_strjoin("foo ", "bar"));
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -32,6 +32,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_atoi(const char *nptr);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 

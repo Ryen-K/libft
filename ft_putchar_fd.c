9a698ccb17ae0ee5ce44e035a224617e78d6bc57
Ryen-K <ryen.kamkoum@gmail.com>
2025-11-02 12:13:16 +0100

    part2: ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*
+int	main(void)
+{
+	char	c;
+
+	c = 'd';
+	ft_putchar_fd(c, 1);
+	ft_putchar_fd('\n', 1);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -45,4 +45,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
+void	ft_putchar_fd(char c, int fd);
+
 #endif

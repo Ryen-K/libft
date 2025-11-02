b7166caee1ae07a6c7f5614edcce3a28a3a69e52
Ryen-K <ryen.kamkoum@gmail.com>
2025-11-02 13:04:53 +0100

    part2: ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + 48, fd);
+}
+/*
+int	main(void)
+{
+	ft_putnbr_fd(25, 1);
+	ft_putchar_fd('\n', 1);
+        ft_putnbr_fd(-25, 1);
+        ft_putchar_fd('\n', 1);
+        ft_putnbr_fd(0, 1);
+        ft_putchar_fd('\n', 1);
+        ft_putnbr_fd(-2147483648, 1);
+        ft_putchar_fd('\n', 1);
+        ft_putnbr_fd(2147483647, 1);
+        ft_putchar_fd('\n', 1);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif

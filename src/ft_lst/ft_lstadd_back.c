056bcb9a8ea433d4f34a1a98d3b9db1e7120d76b
Ryen-K <ryen.kamkoum@gmail.com>
2025-12-20 17:21:29 +0100

    lstadd back & front + last + size + delone + clear: done

--- src/ft_lst/ft_lstadd_back.c
+++ src/ft_lst/ft_lstadd_back.c
@@ -6,8 +6,23 @@
 /*   By: rkamkoum <rkamkoum@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:09:58 by rkamkoum          #+#    #+#             */
-/*   Updated: 2025/12/08 16:10:00 by rkamkoum         ###   ####lausanne.ch   */
+/*   Updated: 2025/12/20 16:49:14 by rkamkoum         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	if (!lst || !new)
+		return ;
+	if (*lst)
+	{
+		last_node = ft_lstlast(*lst);
+		last_node->next = new;
+	}
+	else
+		*lst = new;
+}
--- src/ft_lst/ft_lstdelone.c
+++ src/ft_lst/ft_lstdelone.c
@@ -6,8 +6,16 @@
 /*   By: rkamkoum <rkamkoum@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:10:26 by rkamkoum          #+#    #+#             */
-/*   Updated: 2025/12/08 16:10:28 by rkamkoum         ###   ####lausanne.ch   */
+/*   Updated: 2025/12/20 16:54:37 by rkamkoum         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
